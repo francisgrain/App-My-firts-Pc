8d28327e47285206e4d1d03fcc74f0592af83500
francisgrain <167990276+francisgrain@users.noreply.github.com>
2024-06-05 19:46:29 +0200

    Update MailConfig.java

--- AppPc/src/main/java/com/example/demo/MailConfig.java
+++ AppPc/src/main/java/com/example/demo/MailConfig.java
@@ -16,8 +16,8 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("francescogrande1981@gmail.com");
-        mailSender.setPassword("eczr niam nntr flia");
+        mailSender.setUsername("");
+        mailSender.setPassword("");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
